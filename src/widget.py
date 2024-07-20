52b4ab47c1d40f8c05c8bf31f7e6e0d0a82f9751
Dima <nfuumo@mail.ru>
2024-07-20 19:35:37 +0300

    fixes

--- src/widget.py
+++ src/widget.py
@@ -1,43 +1,43 @@
 from typing import Union
 
+
 def mask_account_card(account: Union[str]) -> str:
+    """Функция маскировки аккаунта."""
     if "Счет" in account:
         mask_account = account[:5] + "*" * 2 + account[21:]
     elif "Maestro" in account:
         mask_account = account[:14] + "*" * 6 + account[20:]
         mask_account = (
-                mask_account[:12] + " " + mask_account[12:16] + " " + mask_account[
-                                                                           16:20] + " " + mask_account[20:]
+            mask_account[:12] + " " + mask_account[12:16] + " " + mask_account[16:20] + " " + mask_account[20:]
         )
     elif "MasterCard" in account:
         mask_account = account[:17] + "*" * 6 + account[23:]
         mask_account = (
-                mask_account[:15] + " " + mask_account[15:19] + " " + mask_account[
-                                                                           19:23] + " " + mask_account[23:]
+            mask_account[:15] + " " + mask_account[15:19] + " " + mask_account[19:23] + " " + mask_account[23:]
         )
     elif "Visa Classic" in account:
         mask_account = account[:18] + "*" * 6 + account[24:]
         mask_account = (
-                mask_account[:16] + " " + mask_account[16:20] + " " + mask_account[
-                                                                   20:24] + " " + mask_account[24:]
+            mask_account[:16] + " " + mask_account[16:20] + " " + mask_account[20:24] + " " + mask_account[24:]
         )
     elif "Visa Platinum" in account:
         mask_account = account[:20] + "*" * 6 + account[26:]
         mask_account = (
-                mask_account[:18] + " " + mask_account[18:22] + " " + mask_account[
-                                                                   22:26] + " " + mask_account[26:]
+            mask_account[:18] + " " + mask_account[18:22] + " " + mask_account[22:26] + " " + mask_account[26:]
         )
     elif "Visa Gold" in account:
         mask_account = account[:16] + "*" * 6 + account[22:]
         mask_account = (
-                mask_account[:14] + " " + mask_account[14:18] + " " + mask_account[
-                                                                   18:22] + " " + mask_account[22:]
+            mask_account[:14] + " " + mask_account[14:18] + " " + mask_account[18:22] + " " + mask_account[22:]
         )
     return mask_account
 
+
 def get_date(date: Union[str]) -> str:
+    """Функция форматирования даты."""
     date_in_the_format = date[8:10] + "." + date[5:7] + "." + date[:4]
     return date_in_the_format
+
 
 if __name__ == "__main__":
     print(mask_account_card("Maestro 1596837868705199"))
