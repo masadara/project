88fd2a3acec93b75539a8515a695071bb05f90a6
Dima <nfuumo@mail.ru>
2024-07-27 16:54:02 +0300

    without tests

---
+++ src/generators.py
@@ -0,0 +1,89 @@
+from typing import Iterator, Union
+
+transactions = [
+    {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
+    },
+    {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    },
+    {
+        "id": 873106923,
+        "state": "EXECUTED",
+        "date": "2019-03-23T01:09:46.296404",
+        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 44812258784861134719",
+        "to": "Счет 74489636417521191160",
+    },
+    {
+        "id": 895315941,
+        "state": "EXECUTED",
+        "date": "2018-08-19T04:27:37.904916",
+        "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод с карты на карту",
+        "from": "Visa Classic 6831982476737658",
+        "to": "Visa Platinum 8990922113665229",
+    },
+    {
+        "id": 594226727,
+        "state": "CANCELED",
+        "date": "2018-09-12T21:27:25.241689",
+        "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод организации",
+        "from": "Visa Platinum 1246377376343588",
+        "to": "Счет 14211924144426031657",
+    },
+]
+
+
+def filter_by_currency(transaction_info: list[dict], currency: Union[str] = "USD") -> Iterator[dict]:
+    for info in transaction_info:
+        if info["operationAmount"]["currency"]["code"] == currency:
+            yield info
+
+
+def transaction_descriptions(transaction_info: list[dict]) -> Iterator[list[dict]]:
+    for info in transaction_info:
+        yield info["description"]
+
+
+def card_number_generator(start: int, end: int) -> Iterator[str]:
+    for number in range(start, end + 1):
+        card_number_gen = str(number)
+        while len(card_number_gen) < 16:
+            card_number_gen = "0" + card_number_gen
+        formatted_card_number = (
+            card_number_gen[:4] + " " + card_number_gen[4:8] + " " + card_number_gen[8:12] + " " + card_number_gen[12:]
+        )
+        yield formatted_card_number
+
+
+if __name__ == "__main__":
+    usd_transactions = filter_by_currency(transactions, "USD")
+    try:
+        for i in range(4):
+            print(next(usd_transactions))
+        print("Конец списка подходящих под фильтрацию операций ")
+    except StopIteration:
+        print("Выход за пределы списка или неверная валюта")
+    descriptions = transaction_descriptions(transactions)
+    try:
+        for i in range(1):
+            print(next(descriptions))
+    except StopIteration:
+        print("Выход за пределы списка или пустой список")
+for card_number in card_number_generator(1, 6):
+    print(card_number)
