8891878b299fa671eac5ce7e34a4b091f943b614
Dima <nfuumo@mail.ru>
2024-08-16 21:51:54 +0300

    homework 12 2

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,32 @@
 import json
 import os
+import logging
 
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+rel_file_path = os.path.join(current_dir, "../logs/utils.log")
+abs_file_path = os.path.abspath(rel_file_path)
+logger = logging.getLogger('utils')
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(abs_file_path, "w", encoding="utf-8")
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 def show_transaction_info(path: str) -> list[dict]:
     """Функция вывода транзакций с JSON файла"""
     transaction_info = []
     try:
+        logger.info(f'Попытка получить транзакции из json файла по пути: {path}')
         with open(path, encoding="utf8") as json_file:
             transaction_info = json.load(json_file)
         print(type(transaction_info))
         return transaction_info
-    except FileNotFoundError:
+    except FileNotFoundError as ex:
+        logger.error(f'Произошла ошибка:{ex}')
         return transaction_info
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as ex:
+        logger.error(f'Произошла ошибка:{ex}')
         return transaction_info
 
 
